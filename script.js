60b68130de389b4061fc2604b5939c89023b6400
as00r <as00r.dev@gmail.com>
2022-11-09 14:49:42 +0530

    Add small screens support, grid size changes
    Now works on small screens, also, a select with 3 choices
    to change grid size added

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 //select Elements
 const WRAPPER = document.querySelector(".wrapper");
 const ROOT = document.querySelector(":root");
+const SELECT = document.querySelector("select");
+const BUTTON = document.querySelector("button");
 
-//256 squares
-for (let i = 0; i < 256; i++) {
-	const SQUARE = document.createElement("div");
-	SQUARE.classList.add("square");
-	WRAPPER.appendChild(SQUARE);
-	//add event listeners to squares
-	SQUARE.addEventListener("mouseover", addColor);
+//Generate Squares
+function generateSquares(gridWidth) {
+	for (let i = 0; i < Math.pow(gridWidth, 2); i++) {
+		const SQUARE = document.createElement("div");
+		SQUARE.classList.add("square");
+		WRAPPER.appendChild(SQUARE);
+		WRAPPER.style.gridTemplateColumns = `repeat(${gridWidth}, 1fr)`;
+		WRAPPER.style.gridTemplateRows = `repeat(${gridWidth}, 1fr)`;
+		//add event listeners to squares
+		SQUARE.addEventListener("mouseover", addColor);
+	}
 }
 
+//clear squares
+function clearSquares() {
+	WRAPPER.innerHTML = "";
+}
 //generate a random color
 function generateColor() {
 	const hue = Math.round(Math.random() * 360);
@@ -22,3 +32,15 @@ function addColor(e) {
 	const hue = generateColor();
 	e.target.style.backgroundColor = `hsl(${hue},100%,50%)`;
 }
+
+//handle click
+function clickHandler() {
+	clearSquares();
+	generateSquares(SELECT.value);
+}
+
+//change grid size when button clicked
+BUTTON.addEventListener("click", clickHandler);
+
+//default 16x16 squares
+generateSquares(16);
